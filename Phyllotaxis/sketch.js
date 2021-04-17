669f0793b29281fe5f80fd976c174030b502a24e
Kamiel <kamieldevisser@gmail.com>
2021-04-17 22:19:19 +0200

    Phyllotaxis optical illusion

--- Phyllotaxis/sketch.js
+++ Phyllotaxis/sketch.js
@@ -2,6 +2,15 @@
 let sketchWidth, sketchHeight;
 let sketchBorderSize = 100;
 
+let points = [];
+let n = 500;
+let alpha = 137.5;
+
+var scalingParameter = 3;
+var petalDiameter = 4;
+
+var petalsAdded = 1;
+
 function setup()
 {
 
@@ -10,10 +19,32 @@ function setup()
 
     createCanvas(sketchWidth, sketchHeight);
 
-    background(0);
+    angleMode(DEGREES);
+    colorMode(HSB);
+
+    console.log(points);
 }
 
-function draw()
-{
+function addPoint(point){
+    points.push(point);
+}
+
+function draw() {
+    background(0);
     fill(255);
+
+    translate(width/2, height/2);
+    rotate(n / 2);
+
+    scalingParameter = mouseY / 100.0;
+    petalsAdded = mouseX / 100;
+
+    for (let i = 0; i < n; i++) {
+        let phi = i * alpha;
+        let distFromCenter = scalingParameter * sqrt(i);
+        fill(phi % 256, n % 256, 255);
+        ellipse(distFromCenter * cos(phi), distFromCenter * sin(phi), petalDiameter);
+    }
+
+    n++;
 }
